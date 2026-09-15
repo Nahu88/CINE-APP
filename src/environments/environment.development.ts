b718283eb10b6afd6ee39018a6d2d861f1d61298
Nahu88 <meliannahu@gmail.com>
2026-09-15 19:52:54 -0300

    build: agregar Supabase y archivos de entorno
    Se instala @supabase/supabase-js y se configura fileReplacements para usar environment.development.ts en desarrollo.

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+// Entorno de desarrollo (usado por `ng serve`)
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://pxyibglkqggaskxqkwjg.supabase.co',
+  supabaseKey: 'sb_publishable_pEtT5S-fNk-j4is4mEYPaw_ORRWMfEk'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+
+export const environment = {
+  production: true,
+  supabaseUrl: 'https://pxyibglkqggaskxqkwjg.supabase.co',
+  supabaseKey: 'sb_publishable_pEtT5S-fNk-j4is4mEYPaw_ORRWMfEk'
+};
